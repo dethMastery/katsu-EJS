1d2d7747e45a608b80c84a4e5a2de5d505a1624c
dethMastery <owner@detzz.in.th>
2022-09-14 15:26:43 +0700

    updated: fuckingly setting up database

--- scripts/page.js
+++ scripts/page.js
@@ -3,12 +3,26 @@ let uConfig = require('./userConfig')
 
 module.exports = function (app, cors, config) {
 
+  // Homepage
   app.get('/', (req, res) => {
     res.render('index', {
       site: uConfig
     })
   })
 
+  // Blog Page
+  app.get('/blog', (req, res) => {
+    let link = mainDB[0].tab[0].blog
+    res.redirect(link)
+  })
+
+  // Artist Page
+  app.get('/artist', (req, res) => {
+    let link = mainDB[0].tab[0].artist
+    res.redirect(link)
+  })
+
+  // API Page
   app.get('/api', cors(config.freeOrigin), (req, res) => {
     res.json(mainDB)
   })
